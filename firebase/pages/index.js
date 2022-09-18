e2f36682595380f6d0a92719b2de1a45cd163a4e
jvs-dev <shepherdcom12@gmail.com>
2022-09-18 02:15:47 -0300

    teste 2

--- firebase/pages/index.js
+++ firebase/pages/index.js
@@ -24,16 +24,11 @@ onAuthStateChanged(auth, (user) => {
 
 
 if (user !== null) {
-  // O objeto de usuário tem propriedades básicas como nome de exibição, email, etc.
-  const displayName = user.displayName;
-  const email = user.email;
-  const photoURL = user.photoURL;
-  const emailVerified = user.emailVerified;
-
-  // O ID do usuário, exclusivo do projeto Firebase. Não use
-  // este valor para autenticar com seu servidor backend, se
-  // você tem um. Use User.getToken() em vez disso.
-  const uid = user.uid;
-  perfilimg.src="https://media.istockphoto.com/photos/trendy-girl-singing-favorite-song-out-loud-in-phone-as-mic-wearing-picture-id1256944025?s=612x612"
-}
-
+    user.providerData.forEach((profile) => {
+      console.log("Sign-in provider: " + profile.providerId);
+      console.log("  Provider-specific UID: " + profile.uid);
+      console.log("  Name: " + profile.displayName);
+      console.log("  Email: " + profile.email);
+      console.log("  Photo URL: " + profile.photoURL);
+    });
+  }
